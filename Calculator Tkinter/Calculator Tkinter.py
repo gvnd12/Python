a887aae5ec69526c925f19d98d6bb235b10d6040
gvnd12 <govindshaju@gmail.com>
2024-03-19 20:28:19 +0530

    Latest

--- Calculator Tkinter/Calculator Tkinter.py
+++ Calculator Tkinter/Calculator Tkinter.py
@@ -1,20 +1,26 @@
 from tkinter import *
-w=Tk()
-w.geometry('250x315')
-w.title('Calculator')
+from tkinter import ttk
 
-ent=Frame()
+w=Tk()
+w.geometry('244x330')
+w.resizable(width=False,height=False)
+w.title('Calculator')
+w.iconbitmap(r'Calculator Tkinter/Icon.ico')
+
+ent=Frame(background='white')
 ent.pack()
 
-btn=Frame()
+btn=Frame(background='#202124')
 btn.pack()
 
-e=Entry(ent,width=24,borderwidth=3,font=('Arial','12'))
-e.pack(side=LEFT,fill=X,expand=TRUE,pady=10,ipady=7)
+e=Entry(ent,width=23,font=('Arial','12'),background='#202124',border=1,foreground='white',insertbackground='white')
+e.pack(side=LEFT,fill=X,expand=TRUE,pady=0,ipady=11,ipadx=17)
 
 def result():
     a=e.get()
-    if '(' or ')' in a:
+    if a=='':
+        e.focus()
+    elif '(' or ')' in a:
         x=a.split('(')
         for i in range(len(x)):
             if ')' in x[i]:
@@ -161,61 +167,62 @@ def barcketclose():
             if a[i]=='(' and a[len(a)-1]!='(':
                 e.insert(END,')')
 
-b1=Button(btn,text='1',command=one,height=2,width=5,font=('Arial','12'))
+b1=Button(btn,text='1',command=one,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 b1.grid(row=3,column=0)
 
-b2=Button(btn,text='2',command=two,height=2,width=5,font=('Arial','12'))
+b2=Button(btn,text='2',command=two,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 b2.grid(row=3,column=1)
 
-b3=Button(btn,text='3',command=three,height=2,width=5,font=('Arial','12'))
+b3=Button(btn,text='3',command=three,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 b3.grid(row=3,column=2)
 
-b4=Button(btn,text='4',command=four,height=2,width=5,font=('Arial','12'))
+b4=Button(btn,text='4',command=four,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 b4.grid(row=4,column=0)
 
-b5=Button(btn,text='5',command=five,height=2,width=5,font=('Arial','12'))
+b5=Button(btn,text='5',command=five,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 b5.grid(row=4,column=1)
 
-b6=Button(btn,text='6',command=six,height=2,width=5,font=('Arial','12'))
+b6=Button(btn,text='6',command=six,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 b6.grid(row=4,column=2)
 
-b7=Button(btn,text='7',command=seven,height=2,width=5,font=('Arial','12'))
+b7=Button(btn,text='7',command=seven,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 b7.grid(row=5,column=0)
 
-b8=Button(btn,text='8',command=eight,height=2,width=5,font=('Arial','12'))
+b8=Button(btn,text='8',command=eight,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 b8.grid(row=5,column=1)
 
-b9=Button(btn,text='9',command=nine,height=2,width=5,font=('Arial','12'))
+b9=Button(btn,text='9',command=nine,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 b9.grid(row=5,column=2)
 
-b0=Button(btn,text='0',command=zero,height=2,width=5,font=('Arial','12'))
+b0=Button(btn,text='0',command=zero,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 b0.grid(row=6,column=1)
 
-barckopen=Button(btn,text='(',command=barcketopen,height=2,width=5,font=('Arial','12'))
-barckopen.grid(row=2,column=0)
+brackopen=Button(btn,text='(',command=barcketopen,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
+brackopen.grid(row=2,column=0)
 
-barckclose=Button(btn,text=')',command=barcketclose,height=2,width=5,font=('Arial','12'))
-barckclose.grid(row=2,column=1)
+brackclose=Button(btn,text=')',command=barcketclose,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
+brackclose.grid(row=2,column=1)
 
-bb=Button(btn,text='<',command=backspace,height=2,width=5,font=('Arial','12'))
+back=PhotoImage(file=r'Calculator Tkinter/Backspace.png')
+bb=Button(btn,image=back,command=backspace,height=55,width=58,background='#202124',activebackground='#303134',border=0)
 bb.grid(row=6,column=2)
 
-br=Button(btn,text='=',command=result,height=2,width=5,font=('Arial','12'))
+br=Button(btn,text='=',command=result,height=2,width=5,font=('Arial','14'),background='#378CE7',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 br.grid(row=6,column=3)
 
-bp=Button(btn,text='+',command=plus,height=2,width=5,font=('Arial','12'))
+bp=Button(btn,text='+',command=plus,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 bp.grid(row=5,column=3)
 
-bs=Button(btn,text='-',command=sub,height=2,width=5,font=('Arial','12'))
+bs=Button(btn,text='-',command=sub,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 bs.grid(row=4,column=3)
 
-bm=Button(btn,text='x',command=mult,height=2,width=5,font=('Arial','12'))
+bm=Button(btn,text='*',command=mult,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 bm.grid(row=3,column=3)
 
-bd=Button(btn,text='/',command=div,height=2,width=5,font=('Arial','12'))
+bd=Button(btn,text='/',command=div,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 bd.grid(row=2,column=3)
 
-bc=Button(btn,text='C',command=clear,height=2,width=5,font=('Arial','12'))
+bc=Button(btn,text='C',command=clear,height=2,width=5,font=('Arial','14'),background='#202124',foreground='white',activeforeground='white',activebackground='#303134',border=0)
 bc.grid(row=2,column=2)
 
 w.mainloop()
